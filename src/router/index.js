b38b091eaed0b929590d30986c78fb9677d2963a
Wilson Jonatan Chay Santizo <tu.email@dominio.com>
2025-09-11 12:57:36 -0600

    fix: Middleware, invited...

--- src/router/index.js
+++ src/router/index.js
@@ -16,19 +16,12 @@ import CatotegorysLayout from '@/layout/user/CatotegorysLayout.vue';
 import EventsView from '@/view/EventsView.vue';
 import PromotionsViewVue from '@/view/PromotionsView.vue';
 import FiltrarCatalogoView from '@/view/FiltrarCatalogoView.vue';
-import SendCodePassword from '@/view//auth/SendCodePassword.vue';
-import Admin from "@/view/admin/admin.vue";
-import Management from "@/view/admin/Management .vue";
-import NewPublication from "@/view/admin/NewPublication.vue"; // Asegúrate de importar cualquier componente necesario
-import NewArticle from "@/view/admin/NewArticle.vue"; 
-import NewArtist from "@/view/admin/NewArtist.vue"; 
-import NewUser from "@/view/admin/NewUser.vue"; 
-import Report from "@/view/admin/Report.vue";  
+import SendCodePassword from '@/view//auth/SendCodePassword.vue'; 
 import CartLayout from '@/layout/user/CartLayout.vue';
 import MyArticles from '@/view/user/cart/MyArticles.vue';
 import WishList from '@/view/user/cart/WishList.vue';
 import Cart from '@/view/user/cart/Cart.vue';
-
+import adminRoutes from './adminRoutes';
 
 
 
@@ -88,63 +81,8 @@ const routes = [
       },
     ],
   },
- 
-
-{
-    path: '/admin', // La ruta base para la sección de admin
-    component: AdminLayout,
-    meta: { requiresAuth: true, role: 'ADMIN' },
-    children: [
-      { 
-        path: 'home', // Ruta completa: '/admin/home'
-        name: 'admin_home', 
-        component: Admin 
-      },
-    { 
-        path: 'articles', // Ruta completa: '/admin/management'
-        name: 'admin_article', 
-        component: NewArticle
-      },
-        { 
-        path: 'artist', // Ruta completa: '/admin/management'
-        name: 'admin_artist', 
-        component: NewArtist 
-      },
-    { 
-        path: 'promotions', // Ruta completa: '/admin/management'
-        name: 'admin_promotions', 
-        component: NewArtist 
-      },
-        { 
-        path: 'events', // Ruta completa: '/admin/management'
-        name: 'admin_events', 
-        component: Management 
-      },
-        { 
-        path: 'users', // Ruta completa: '/admin/management'
-        name: 'admin_users', 
-        component: NewUser
-      },
-        { 
-        path: 'reports', // Ruta completa: '/admin/management'
-        name: 'admin_reports', 
-        component: Report 
-      },
-      { 
-        path: 'new-publication', // Ejemplo de una ruta adicional
-        name: 'admin_new_publication', 
-        component: NewPublication 
-      },
-      // Puedes agregar más rutas de admin aquí
-    ]
-  },
-
-
-
-
-
-
-
+  
+  ...adminRoutes,
 
 
  
