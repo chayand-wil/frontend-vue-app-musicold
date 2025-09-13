333e82063bb45af6bdd399c239aa7d3b7867032d
Dou <doueduar2018@gmail.com>
2025-09-12 21:30:44 -0600

    add proyect

--- src/services/event.js
+++ src/services/event.js
@@ -23,3 +23,50 @@ export const fetchAllEvents = async () => {
     throw error; // Re-lanzar el error para manejarlo en el componente
   }
 };
+
+/**
+ * Crea un nuevo artista.
+ * @param {Object} artistData Los datos del artista a crear.
+ * @returns {Promise<Object>} Una promesa que resuelve con el artista creado.
+ */
+export const createEvents = async (artistData) => {
+  try {
+    const response = await api.post(API_URL, artistData);
+    return response.data;
+  } catch (error) {
+    console.error('Error en createArtist:', error);
+    throw error;
+  }
+};
+
+/**
+ * Actualiza un artista existente.
+ * @param {number} id El ID del artista.
+ * @param {Object} artistData Los datos del artista a actualizar.
+ * @returns {Promise<Object>} Una promesa que resuelve con el artista actualizado.
+ */
+export const updateEvents = async (id, artistData) => {
+  try {
+    const response = await api.put(`${API_URL}/${id}`, artistData);
+    return response.data;
+  } catch (error) {
+    console.error('Error en updateArtist:', error);
+    throw error;
+  }
+};
+
+/**
+ * Actualiza un artista existente.
+ * @param {number} id El ID del artista.
+ * @param {Object} artistData Los datos del artista a actualizar.
+ * @returns {Promise<Object>} Una promesa que resuelve con el artista actualizado.
+ */
+export const deleteEvents = async (id) => {
+  try {
+    const response = await api.delete(`${API_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error en updateArtist:', error);
+    throw error;
+  }
+};
